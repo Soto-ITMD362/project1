15ebce2ebb4df7f164d4c1c8b10d68491428fff7
msoto6 <70609219+msoto6@users.noreply.github.com>
2021-03-16 20:31:19 -0500

    Update function in sites

--- js/site.js
+++ js/site.js
@@ -4,3 +4,15 @@ jQuery(function($) {
   $('html').addClass('hasjs');
 });
 
+function validateForm() {
+  var w = document.forms["beautybakerie"]["name"].value;
+  var x = document.forms["beautybakerie"]["mail"].value;
+  var y = document.forms["beautybakerie"]["phone"].value;
+  var z = document.forms["beautybakerie"]["birthday"].value;
+  if ((w == "")||(x == "")||(y == "")||(z == "")){
+    alert("Please fill in the missing entries");
+	}
+  else {
+  	alert("Thank you for joining the Bakerie! See you soon with details in your inbox!")
+  }
+}
